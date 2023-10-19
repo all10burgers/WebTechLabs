a805a35fc015ad2bccb1a2247602d2cc51d5c57b
all10burgers <travisalten@gmail.com>
2023-10-19 15:55:28 -0500

    lab8

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,5 +1,23 @@
 
 function getDataFromForm() {
-  alert("it worked!");
-  
+  var fname = document.getElementsByName('fname')[0].value;
+  var lname = document.getElementsByName('lname')[0].value;
+
+  runAjax(fname, lname);
+}
+
+function runAjax(fname, lname) {
+  var xhr = new XMLHttpRequest();
+  var url = 'ajax.php?fname=' + encodeURIComponent(fname) + '&lname=' + encodeURIComponent(lname);
+
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          var response = xhr.responseText;
+          document.getElementById('responseString').innerText = response;
+      }
+  };
+
+  xhr.send();
 }
